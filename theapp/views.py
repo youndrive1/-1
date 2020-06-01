0cc470e66ad70e6cbf00856604041396441edd2b
노윤진 <youndrive1@gmail.com>
2020-06-02 01:34:14 +0900

    색

---
+++ theapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def main(requests):
+    return render(requests, 'main.html')
+
+def photos(requests):
+    return render(requests, 'photos.html')
+    
+def intro(requests):
+    return render(requests, 'intro.html')
